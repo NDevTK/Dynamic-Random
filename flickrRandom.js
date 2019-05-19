231963b4425e7f8e56427e5c0168958e7f49a129
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-05-19 16:53:34 +0100

    Update flickrRandom.js

--- flickrRandom.js
+++ flickrRandom.js
@@ -107,6 +107,10 @@ function event(data) { // Main callback from flickr (returns true if event)
         alert(error);
         console.log(error);
     }
+    if (!data.photos.photo[0].url_o){
+        GetImage();
+        return false;
+    }
     FlickrRND.pages = data.photos.pages; // Get total pages
     if (FlickrRND.state > FlickrRND.pages) {
         FlickrRND.state = 0; // If state is invalid reset to 0
@@ -118,7 +122,7 @@ function event(data) { // Main callback from flickr (returns true if event)
         setInterval(SendEvent, FlickrRND.update_rate);
         if (FlickrRND.state > 0) return false // Dont send event
     }
-    if (data.photos.photo[0].url_o && data.photos.photo[0].owner) { // Check response has url_o, owner
+    if (data.photos.photo[0].url_o && data.photos.photo[0].owner) {
         FlickrRND.queue.push({
             detail: {
                 url: data.photos.photo[0].url_o,

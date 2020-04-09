525e63a3ea35b672ac8f465be5f3551f2b1f5ec1
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-04-09 22:21:43 +0100

    Update images.js

--- images.js
+++ images.js
@@ -1,26 +1,19 @@
-  
 FlickrRND.JSONP = false;
 FlickrRND.per_event = 1;
-oldCount = 1;
 
-function CreateURL(page){ // Override from flickrRandom
+function CreateURL(page) {
     return "https://imgapi.ndev.tk/" + FlickrRND.subject + "/" + page;
 }
 
 let params = (new URL(document.location)).searchParams;
-
-// Unsplash Start
 unsplash = params.has('unsplash');
 subject = encodeURI(params.get('subject'));
-
 type = (params.has('subject')) ? "featured" : "random";
-
-res =  window.screen.availHeight+"x"+window.screen.availWidth;
-
+res = window.screen.availHeight + "x" + window.screen.availWidth;
 src = "https://source.unsplash.com/" + type + "/" + res;
 
-if(params.has('subject')) {
-    src = src.concat("/?"+subject);
+if (params.has('subject')) {
+    src = src.concat("/?" + subject);
 }
 
 function sleep(ms) {
@@ -29,35 +22,28 @@ function sleep(ms) {
 
 async function imagemgr() {
     while (true) {
-        if(bg.images.length < 3) {
+        if (bg.images.length < 3) {
             await fetch(src).then(img => addImage(img.url));
             await sleep(3000);
         }
     }
 }
 
-
 function addImage(url) {
-	if(bg.images.length > 3) return
-        bg.images.push(url);
-        // Cleanup images
-	if(bg._zCounter > oldCount) {
-	    while(bg._zCounter > oldCount) {
-	        bg.images.shift();
-            }
-	    oldCount = bg._zCounter;
-	}
+    bg.images.push(url);
+    while(bg.images.length > 3) {
+		bg.images.shift();
+    }
 }
 
 function Start() {
-	return console.warn("Dynamic-Random NOT WORKING :-(");
-	bg.images = [src];
-	if (unsplash) {
-		imagemgr(); // Unsplash
-	} else {
-		window.addEventListener("onFlickrImage", function(event) { // Flickr
-			addImage(event.detail.urls[0]);
-		})
-		InitFlickrRandom(subject);
-	}
+    bg.images = [src];
+    if (unsplash) {
+        imagemgr();
+    } else {
+        window.addEventListener("onFlickrImage", function (event) {
+            addImage(event.detail.urls[0]);
+        })
+        InitFlickrRandom(subject);
+    }
 }

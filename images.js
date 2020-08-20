3e5c42481e1bc99a26b4958143e6b30536aa5f3c
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-08-20 02:09:03 +0100

    Update images.js

--- images.js
+++ images.js
@@ -29,7 +29,7 @@ async function imagemgr() {
 
 function addImage(url) {
     bg.images.push(url);
-    while(bg.images.length > 2) {
+    while(bg.images.length > 3) {
         bg.images.shift();
     }
 }

647b99382548754ac70639397f2efe3ba61ebb89
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-05 03:48:29 +0000

    Update images.js

--- images.js
+++ images.js
@@ -5,13 +5,15 @@ type = (params.has('subject')) ? "featured" : "random";
 res = window.screen.availHeight + "x" + window.screen.availWidth;
 
 function Start() {
+    var prefix = "?";
     if (unsplash) {
 	    src = "https://source.unsplash.com/" + type + "/" + res;
 	    if (params.has('subject')) {
-		    src = src.concat("/?" + subject);
+		    src = src.concat("/"+ prefix + subject);
+		    prefix = "&";
 	    }
     } else {
 	    src = "https://imgapi.ndev.workers.dev/?subject="+subject;
     }
-    bg.images = Array.from({length: 100000}, (_, i) => src+"&c=" + i + 1);
+    bg.images = Array.from({length: 100000}, (_, i) => src + prefix + "c=" + i + 1);
 }

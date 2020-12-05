45beeb4375b93edb70414c9d9a0acadc3ccebaea
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-05 04:02:30 +0000

    Update images.js

--- 360/images.js
+++ 360/images.js
@@ -5,22 +5,22 @@ type = (params.has('subject')) ? "featured" : "random";
 
 function Start() {
     var prefix = "&";
-    if (unsplash) {	    
-	    if (params.has('subject')) {
-		    src = src.concat("/?"+subject);
-	    } else {
+    if (unsplash) {
+        if (params.has('subject')) {
+            src = src.concat("/?" + subject);
+        } else {
             src = "https://source.unsplash.com/" + type;
             prefix = "?";
         }
     } else {
-	    src = "https://imgapi.ndev.workers.dev/?subject="+subject;
+        src = "https://imgapi.ndev.workers.dev/?subject=" + subject;
     }
     var i = 1;
     bg.setAttribute("height", window.screen.availHeight);
-    bg.setAttribute("width", window.screen.availWidth); 
+    bg.setAttribute("width", window.screen.availWidth);
     bg.setAttribute("src", src + prefix + "c=" + i);
     setInterval(_ => {
-      bg.setAttribute("src", src + prefix + "c=" + i);
-      i += 1;
+        bg.setAttribute("src", src + prefix + "c=" + i);
+        i += 1;
     }, 5000);
 }

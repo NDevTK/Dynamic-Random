8318db40b057620b6681628c11d7ed4759a51df7
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-11-23 13:58:04 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,5 @@ const headers = new Headers(response.headers);
 headers.set('X-Frame-Options', 'ALLOWALL');
 //headers.set('Cross-Origin-Opener-Policy', 'same-origin');
 headers.set('Strict-Transport-Security', 'max-age=31536000');
-headers.set('X-Content-Type-Options', 'nosniff')
 return new Response(response.body, { headers: headers });
 }

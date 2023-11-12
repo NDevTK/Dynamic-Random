c1e5c6b81909fcb2468f7c4d95bb8d479a1cc9e7
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-11-12 05:23:05 +0000

    Delete sw directory

--- sw/sw.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request).then(inject));
-});
-
-function inject(response) {
-const headers = new Headers(response.headers);
-return new Response(response.body, { headers: headers });
-}

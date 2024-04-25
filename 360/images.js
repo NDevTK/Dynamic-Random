c2ab795788884cb19253230b6826aeb86a3d2102
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-04-25 04:04:35 +0100

    Update images.js

--- 360/images.js
+++ 360/images.js
@@ -1,14 +1,10 @@
-if (window !== window.top && window.origin !== 'null') {
-  console.error('Please use a sandboxed iframe');
-  return
-}
-
 let params = (new URL(document.location)).searchParams;
 unsplash = params.has('unsplash');
 subject = (params.has('subject')) ? encodeURI(params.get('subject')) : "nature";
 type = (params.has('subject')) ? "featured" : "random";
 
 function Start() {
+    if (window !== window.top && window.origin !== 'null') { console.error('Please use a sandboxed iframe'); return }
     var prefix = "&";
     if (unsplash) {
         src = "https://source.unsplash.com/" + type;
